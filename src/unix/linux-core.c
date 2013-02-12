e9d0496d8b9a913d4e56d01569b0683e1e60a17f
Jameson Nash <vtjnash@gmail.com>
2013-02-12 13:01:55 -0500

    quasi-support for file handles as pipe objects on linux

--- src/unix/linux-core.c
+++ src/unix/linux-core.c
@@ -195,14 +195,18 @@ void uv__io_poll(uv_loop_t* loop, int timeout) {
      * events, skip the syscall and squelch the events after epoll_wait().
      */
     if (uv__epoll_ctl(loop->backend_fd, op, w->fd, &e)) {
-      if (errno != EEXIST)
+      if (errno == EPERM) { /* fd is probably a file, which is always ready */
+        w->cb(loop, w, UV__EPOLLIN | UV__EPOLLOUT);
+        timeout = 0; /* fd is now likely closed and needs to return to the uv_run loop for cleanup */
+      } else if (errno != EEXIST) {
         abort();
-
+      } else {
       assert(op == UV__EPOLL_CTL_ADD);
 
       /* We've reactivated a file descriptor that's been watched before. */
       if (uv__epoll_ctl(loop->backend_fd, UV__EPOLL_CTL_MOD, w->fd, &e))
         abort();
+    }
     }
 
     w->events = w->pevents;
