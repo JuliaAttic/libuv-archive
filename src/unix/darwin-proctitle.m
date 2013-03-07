1c5b9d80a08aa8b418cc8e57e99dc25f785805d1
Jameson Nash <vtjnash@gmail.com>
2013-03-06 21:40:56 -0500

    disable setting process title on Mac (avoid needing to link in ApplicationServices.framework for now)

--- src/unix/darwin-proctitle.m
+++ src/unix/darwin-proctitle.m
@@ -22,6 +22,7 @@
 
 
 int uv__set_process_title(const char* title) {
+/*
   typedef CFTypeRef (*LSGetCurrentApplicationASNType)(void);
   typedef OSStatus (*LSSetApplicationInformationItemType)(int,
                                                           CFTypeRef,
@@ -63,16 +64,18 @@ int uv__set_process_title(const char* title) {
   if (display_name_key == NULL || *display_name_key == NULL)
     return -1;
 
-  /* Force the process manager to initialize. */
+  /* Force the process manager to initialize. * /
   GetCurrentProcess(&psn);
 
   display_name = CFStringCreateWithCString(NULL, title, kCFStringEncodingUTF8);
   asn = ls_get_current_application_asn();
-  err = ls_set_application_information_item(-2,  /* Magic value. */
+  err = ls_set_application_information_item(-2,  /* Magic value. * /
                                             asn,
                                             *display_name_key,
                                             display_name,
                                             NULL);
 
   return (err == noErr) ? 0 : -1;
+  */
+  return -1;
 }
