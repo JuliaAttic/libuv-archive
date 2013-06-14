ef7d00abc1550a6d5a7b4b9b56d16e5e5e990491
Keno Fischer <kfischer@college.harvard.edu>
2013-06-14 15:37:21 -0400

    fix build on unix

--- src/unix/internal.h
+++ src/unix/internal.h
@@ -228,7 +228,7 @@ void uv__tcp_close(uv_tcp_t* handle);
 void uv__timer_close(uv_timer_t* handle);
 void uv__udp_close(uv_udp_t* handle);
 void uv__udp_finish_close(uv_udp_t* handle);
-void uv__finish_close(uv_handle_t* handle);
+void uv__finish_close(uv_handle_t *handle);
 
 #if defined(__APPLE__)
 int uv___stream_fd(uv_stream_t* handle);
