c45ec4715ecaf6f5ae01e8a760db54283e9b27d5
Jameson Nash <vtjnash@gmail.com>
2013-06-18 18:34:36 -0400

    protect uv_spawn from signals (fix timholy/Profile.jl#16)

--- src/unix/process.c
+++ src/unix/process.c
@@ -260,7 +260,7 @@ static void uv__process_child_init(uv_process_options_t options,
   _exit(127);
 }
 
-
+#include <signal.h>
 int uv_spawn(uv_loop_t* loop,
              uv_process_t* process,
              const uv_process_options_t options) {
@@ -278,6 +278,10 @@ int uv_spawn(uv_loop_t* loop,
                              UV_PROCESS_SETUID |
                              UV_PROCESS_WINDOWS_HIDE |
                              UV_PROCESS_WINDOWS_VERBATIM_ARGUMENTS)));
+  
+  sigset_t sigset, sigoset;
+  sigfillset(&sigset);
+  sigprocmask(SIG_SETMASK, &sigset, &sigoset);
 
   uv__handle_init(loop, (uv_handle_t*)process, UV_PROCESS);
   QUEUE_INIT(&process->queue);
@@ -324,7 +328,7 @@ int uv_spawn(uv_loop_t* loop,
     goto error;
 
   uv_signal_start(&loop->child_watcher, uv__chld, SIGCHLD);
-
+  
   pid = fork();
 
   if (pid == -1) {
@@ -334,6 +338,7 @@ int uv_spawn(uv_loop_t* loop,
   }
 
   if (pid == 0) {
+    sigprocmask(SIG_SETMASK, &sigoset, NULL);
     uv__process_child_init(options, stdio_count, pipes, signal_pipe[1]);
     abort();
   }
@@ -364,6 +369,7 @@ int uv_spawn(uv_loop_t* loop,
   uv__handle_start(process);
 
   free(pipes);
+  sigprocmask(SIG_SETMASK, &sigoset, NULL);
   return 0;
 
 error:
@@ -374,6 +380,7 @@ error:
       close(pipes[i]);
   }
   free(pipes);
+  sigprocmask(SIG_SETMASK, &sigoset, NULL);
 
   return -1;
 }
