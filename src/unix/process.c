16a5e2b9285e7e8f3f0b2403e56cf1ac3001496c
Keno Fischer <kfischer@college.harvard.edu>
2013-06-21 17:26:35 -0400

    Add option to reset SIGPIPE in child process

--- src/unix/process.c
+++ src/unix/process.c
@@ -277,8 +277,15 @@ static void uv__process_child_init(const uv_process_options_t* options,
     _exit(127);
   }
 
+  if ((options.flags & UV_PROCESS_RESET_SIGPIPE) && signal(SIGPIPE,SIG_DFL) == SIG_ERR)
+  {
+    uv__write_int(error_fd, errno);
+    perror("signal()");
+    _exit(127);
+  }
+
   if (options->env != NULL) {
-    environ = options->env;
+    environ = options.env;
   }
 
   execvp(options->file, options->args);
@@ -314,8 +321,9 @@ int uv_spawn(uv_loop_t* loop,
                              UV_PROCESS_SETGID |
                              UV_PROCESS_SETUID |
                              UV_PROCESS_WINDOWS_HIDE |
-                             UV_PROCESS_WINDOWS_VERBATIM_ARGUMENTS)));
-  
+                             UV_PROCESS_WINDOWS_VERBATIM_ARGUMENTS |
+                             UV_PROCESS_RESET_SIGPIPE)));
+
   sigset_t sigset, sigoset;
   sigfillset(&sigset);
   sigprocmask(SIG_SETMASK, &sigset, &sigoset);
