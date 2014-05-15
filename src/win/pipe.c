9cb5b5310752e25c5c8aebca8e6c890f9b27163f
Jameson Nash <vtjnash@gmail.com>
2014-05-15 01:40:44 -0400

    reverse FILE_*_DATA to UV_HANDLE_*ABLE mapping

--- src/win/pipe.c
+++ src/win/pipe.c
@@ -2049,9 +2049,9 @@ int uv_pipe_open(uv_pipe_t* pipe, uv_file file) {
   uv_pipe_connection_init(pipe);
 
   if (access.AccessFlags&FILE_WRITE_DATA)
-    pipe->flags |= UV_HANDLE_READABLE;
-  if (access.AccessFlags&FILE_READ_DATA)
     pipe->flags |= UV_HANDLE_WRITABLE;
+  if (access.AccessFlags&FILE_READ_DATA)
+    pipe->flags |= UV_HANDLE_READABLE;
 
   if (pipe->flags&UV_HANDLE_PIPE_IPC) {
     assert(!(pipe->flags & UV_HANDLE_NON_OVERLAPPED_PIPE));
