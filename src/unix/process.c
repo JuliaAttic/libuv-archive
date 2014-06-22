e2549db176916b14969751f7548e33d4cfc51899
Tony Kelman <tony@kelman.net>
2014-06-21 22:39:30 -0700

    Fix execvpe not found error for glibc < 2.11
    ref #24
    This implementation is from musl-libc, which is MIT licensed

--- src/unix/process.c
+++ src/unix/process.c
@@ -44,6 +44,46 @@ extern char **environ;
 
 #if defined(__linux__) || defined(__GLIBC__)
 # include <grp.h>
+# ifdef __GLIBC_PREREQ
+#  if ! (__GLIBC_PREREQ(2,11))
+int execvpe(const char *file, char *const argv[], char *const envp[])
+{
+	const char *p, *z, *path = getenv("PATH");
+	size_t l, k;
+
+	errno = ENOENT;
+	if (!*file) return -1;
+
+	if (strchr(file, '/'))
+		return execve(file, argv, envp);
+
+	if (!path) path = "/usr/local/bin:/bin:/usr/bin";
+	k = strnlen(file, NAME_MAX+1);
+	if (k > NAME_MAX) {
+		errno = ENAMETOOLONG;
+		return -1;
+	}
+	l = strnlen(path, PATH_MAX-1)+1;
+
+	for(p=path; ; p=z) {
+		char b[l+k+1];
+		z = strchr(p, ':');
+		if (!z) z = p+strlen(p);
+		if (z-p >= l) {
+			if (!*z++) break;
+			continue;
+		}
+		memcpy(b, p, z-p);
+		b[z-p] = '/';
+		memcpy(b+(z-p)+(z>p), file, k+1);
+		execve(b, argv, envp);
+		if (errno != ENOENT) return -1;
+		if (!*z++) break;
+	}
+	return -1;
+}
+#  endif
+# endif
 #endif
 
 #if defined(__linux__)
