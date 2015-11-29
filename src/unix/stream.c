fa40fcac0065a9ee7f24f02f762b6b46ee9327a4
Jameson Nash <vtjnash@gmail.com>
2015-11-29 12:16:14 -0500

    unix: in uv_shutdown, remove call to shutdown syscall
    julia is using uv_shutdown to gracefully close streams (after flushing),
    the shutdown syscall closes the stream for all writers,
    which may include other processes (for example, if the stream was inherited as STDIO)

--- src/unix/stream.c
+++ src/unix/stream.c
@@ -685,8 +685,8 @@ static void uv__drain(uv_stream_t* stream) {
     uv__req_unregister(stream->loop, req);
 
     err = 0;
-    if (shutdown(uv__stream_fd(stream), SHUT_WR))
-      err = UV__ERR(errno);
+    //if (shutdown(uv__stream_fd(stream), SHUT_WR))
+    //  err = -errno;
 
     if (err == 0)
       stream->flags |= UV_STREAM_SHUT;
